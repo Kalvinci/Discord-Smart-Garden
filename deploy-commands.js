aa4c1f41e9e966615ec709fe90df84c12466d6e3
Kalvinci <kalyanasundharam97@gmail.com>
2022-03-29 18:22:09 -0400

    env variables references changed

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-const { token, guildId, clientId } = require("./config.json");
 
 const commands = [
 	new SlashCommandBuilder()
@@ -9,8 +8,14 @@ const commands = [
 		.setDescription("Lists all plants in your Smart Garden"),
 ].map((command) => command.toJSON());
 
-const rest = new REST({ version: "9" }).setToken(token);
+const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(
+	Routes.applicationGuildCommands(
+		process.env.CLIENT_ID,
+		process.env.GUILD_ID
+	),
+	{ body: commands }
+)
 	.then(() => console.log("Successfully registered application commands."))
 	.catch(console.error);

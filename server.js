4d03e8a159c49bdaa38d977e6d28cfa57109598c
Kalvinci <kalyanasundharam97@gmail.com>
2022-03-29 18:13:07 -0400

    typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-	res.send("Bot Server running :)");
+	res.send("Bot Server Running :)");
 });
 
 app.listen(PORT);
